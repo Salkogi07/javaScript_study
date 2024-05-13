3df87da7a440378eb32c44f1dca12ab964cf8b0a
kogsial <kogisal099@gmail.com>
2024-05-13 16:01:37 +0900

    feat:Basic HTML

--- 5_13_Memory/app.js
+++ 5_13_Memory/app.js
@@ -50,10 +50,13 @@ const cards = [
 ];
 
 document.addEventListener("DOMContentLoaded",() => {
-    let cardsChosen = []
-    let cardsChosenId = []
     cards.sort(() => 0.5 - Math.random());
     const grid = document.querySelector(".grid");
+    const resultDisplay = document.querySelector("#result");
+    let cardsChosen = [];
+    let cardsChosenId = [];
+    let cardsWon = [];
+
     function createBoard(){
         for(let i = 0; i < cards.length; i++){
             const card = document.createElement("img");
@@ -63,6 +66,7 @@ document.addEventListener("DOMContentLoaded",() => {
             grid.appendChild(card);
         }
     }
+
     function flipCard(){
         let cardId = this.getAttribute("data-id");
         cardsChosen.push(cards[cardId].name);
@@ -77,10 +81,32 @@ document.addEventListener("DOMContentLoaded",() => {
         const cards = document.querySelectorAll("img");
         const optionOneId = cardsChosenId[0];
         const optionTwoId = cardsChosenId[1];
-        cards[optionOneId].setAttribute("src","image/hidden_card.png");
-        cards[optionTwoId].setAttribute("src","image/hidden_card.png");
+
+        if(optionOneId == optionTwoId){
+            cards[optionOneId].setAttribute("src","image/hidden_card.png");
+            cards[optionTwoId].setAttribute("src","image/hidden_card.png");
+            alert("You hvae clicked the same image!");
+        }
+        else if(cardsChosen[0] === cardsChosen[1]){
+            alert("You found a match");
+            cards[optionOneId].setAttribute("src","image/white_card.png");
+            cards[optionTwoId].setAttribute("src","image/white_card.png");
+            cards[optionOneId].removeEventListener("click",flipCard);
+            cards[optionTwoId].removeEventListener("click",flipCard);
+            cardsWon.push(cardsChosen);
+        }
+        else{
+            cards[optionOneId].setAttribute("src","image/hidden_card.png");
+            cards[optionTwoId].setAttribute("src","image/hidden_card.png");
+            alert("Sorry, try again");
+        }
         cardsChosen = [];
         cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if(cardsWon.length === cards.length/2){
+            resultDisplay.textContent = "Congratulations! You found them all!";
+        }
     }
+
     createBoard();
 });
