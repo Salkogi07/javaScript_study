ffe5b1d29adb830ffa16032fa9b61e4119dc3572
kogsial <kogisal099@gmail.com>
2024-06-03 14:13:04 +0900

    Snake Game

--- 5_13_duduge/app.js
+++ 5_13_duduge/app.js
@@ -2,8 +2,23 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 const grid = document.querySelector(".grid");
 
+const mole = document.querySelector(".mole");
+
 for (let i=0; i<9; i++){
     const square = document.createElement("div");
     square.classList.add("square");
     grid.appendChild(square);
+}
+
+const squares = document.querySelectorAll(".square");
+squares[0].classList.add("mole");
+
+randomSquare();
+
+function randomSquare(){
+    squares.forEach(function(square){
+        square.classList.remove('mole')
+    })
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
 }
