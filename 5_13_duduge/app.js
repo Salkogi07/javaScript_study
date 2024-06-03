ceca2d7548cf993569e7508788aab75568222c20
kogsial <kogisal099@gmail.com>
2024-06-03 15:13:03 +0900

    Snake Game

--- 5_13_duduge/app.js
+++ 5_13_duduge/app.js
@@ -1,23 +1,44 @@
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 const grid = document.querySelector(".grid");
-
 const mole = document.querySelector(".mole");
+var squares;
+let result = 0;
+let targetPosition = null;
+let currentTime = 60;
+let timer;
+let countdownTimer;
 
 for (let i=0; i<9; i++){
     const square = document.createElement("div");
     square.classList.add("square");
     grid.appendChild(square);
 }
+squares = document.querySelectorAll(".square");
 
-const squares = document.querySelectorAll(".square");
-
-randomSquare();
+squares[0].classList.add("mole")
 
 function randomSquare(){
-    squares.forEach(function(square){
-        square.classList.remove('mole')
+    squares.forEach(function(square) {
+        square.classList.remove("mole")
     })
-    let randomSquare = squares[Math.floor(Math.random() * 9)]
-    randomSquare.classList.add('mole')
+    let randomNum = Math.floor(Math.random() * 9)
+    targetPosition = randomNum
+    squares[randomNum].classList.add("mole")
+}
+
+function moveInterval(){
+    timer = setInterval(randomSquare,1000);
+}
+
+moveInterval()
+
+for(let i=0; i < 9; i++){
+    squares[i].addEventListener('mousedown',function(){
+        if(i === targetPosition){
+            result++;
+            score.textContent = result;
+            targetPosition = null;
+        }
+    })
 }
