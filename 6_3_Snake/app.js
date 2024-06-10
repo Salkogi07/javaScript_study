44321d7cae32b5a5c6a47b7e86bd3ef1c80b6f9f
kogsial <kogisal099@gmail.com>
2024-06-10 14:47:52 +0900

    Snake_Game

--- 6_3_Snake/app.js
+++ 6_3_Snake/app.js
@@ -10,19 +10,28 @@ const stopBtn = document.querySelector("#stopBtn");
 const scoreDisplay = document.querySelector("#score");
 const width = 10;
 
-let snake = 0;
+let snake = [2,1,0];
 let interval;
-let intervalTime = 1000;
+let intervalTime = 300;
 let dir = 1;
+let appleLoc = 0;
 
 function start(){
-    squares[snake].classList.add("snake");
+    snake.forEach(function(item){
+        squares[item].classList.add("snake");
+    });
+    makeApple();
     interval = setInterval(runGame,intervalTime);
 }
+function makeApple(){
+    appleLoc = Math.floor(Math.random() * squares.length);
+    squares[appleLoc].classList.add("apple");
+}
 function runGame(){
-    squares[snake].classList.remove("snake");
-    snake = snake + dir;
-    squares[snake].classList.add("snake");
+    const tail = snake.pop();
+    squares[tail].classList.remove("snake");
+    snake.unshift(snake[0]+dir);
+    squares[snake[0]].classList.add("snake");
 }
 function move(e){
     if(e.keyCode == 37){dir = -1}
