c89f53fdde989af5d24e837b9f2196c4e4516fc6
kogsial <kogisal099@gmail.com>
2024-06-10 15:05:11 +0900

    Snake_Game

--- 6_3_Snake/app.js
+++ 6_3_Snake/app.js
@@ -12,9 +12,10 @@ const width = 10;
 
 let snake = [2,1,0];
 let interval;
-let intervalTime = 300;
+let intervalTime = 1000;
 let dir = 1;
 let appleLoc = 0;
+let socore = 0;
 
 function start(){
     snake.forEach(function(item){
@@ -23,14 +24,20 @@ function start(){
     makeApple();
     interval = setInterval(runGame,intervalTime);
 }
-function makeApple(){
-    appleLoc = Math.floor(Math.random() * squares.length);
-    squares[appleLoc].classList.add("apple");
-}
 function runGame(){
-    const tail = snake.pop();
-    squares[tail].classList.remove("snake");
     snake.unshift(snake[0]+dir);
+    if(squares[snake[0]].classList.contains("apple")){
+        intervalTime = intervalTime * 0.9;
+        clearInterval(interval);
+        interval = setInterval(runGame,intervalTime);
+        squares[snake[0]].classList.remove("apple");
+        squares[snake[0]].classList.add("snake");
+        makeApple();
+    }
+    else{
+        const tail = snake.pop();
+        squares[tail].classList.remove("snake");
+    }
     squares[snake[0]].classList.add("snake");
 }
 function move(e){
@@ -39,5 +46,9 @@ function move(e){
     else if(e.keyCode === 38){dir = -width}
     else if(e.keyCode === 40){dir = width}
 }
-document.addEventListener("keyup",move)
+function makeApple(){
+    appleLoc = Math.floor(Math.random() * squares.length);
+    squares[appleLoc].classList.add("apple");
+}
+document.addEventListener("keyup",move);
 startBtn.addEventListener("click",start);
