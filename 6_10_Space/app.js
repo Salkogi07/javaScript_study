5aca989a9c5e2d4a293fc359a519dbc308e45b74
salkogi07 <kogisal099@gmail.com>
2024-06-24 14:13:05 +0900

    spaceGame

--- 6_10_Space/app.js
+++ 6_10_Space/app.js
@@ -5,8 +5,66 @@ for(let i=0; i < stageSize*stageSize; i++){
     document.querySelector(".grid").appendChild(box);
 }
 
-const grid = document.querySelector(".gird");
+const grid = document.querySelector(".grid");
 const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
+
+let playerLoc = stageSize*(stageSize-2)+7
+
+function makePlayer(){
+    stage[playerLoc].classList.add("player");
+}
+makePlayer()
+
+function movePlayer(e){
+    stage[playerLoc].classList.remove("player");
+    switch(e.keyCode){
+        case 37:
+            if(playerLoc % stageSize !== 0){
+                playerLoc--;
+            }
+            break;
+        case 39:
+            if(playerLoc % stageSize < stageSize-1){
+                playerLoc++;
+            }
+            break;
+        case 40: // 아래쪽 화살표
+            if (playerLoc + stageSize < stageSize * stageSize) {
+                playerLoc += 15;
+            }
+            break;
+        case 38: // 위쪽 화살표
+            if (playerLoc - stageSize >= 0) {
+                playerLoc -= 15;
+            }
+            break;
+    }
+    stage[playerLoc].classList.add("player");
+    console.log(playerLoc)
+}
+
+document.addEventListener("keyup",movePlayer)
+
+let invaderLoc = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+]
+let gameInterval;
+
+function makeInvader(){
+    invaderLoc.forEach(function(invader){
+        stage[invader].classList.add("invader");
+    })
+}
+makeInvader();
+
+function moveInvader(){
+
+}
+
+interval = setInterval(moveInvader,1000)
+
