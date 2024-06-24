a31080763567c2875c942e81c8e4d8bcc32066e4
salkogi07 <kogisal099@gmail.com>
2024-06-24 15:28:51 +0900

    spaceGame

--- 6_10_Space/app.js
+++ 6_10_Space/app.js
@@ -11,22 +11,27 @@ const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
 
-let playerLoc = stageSize*(stageSize-2)+7
+let playerLoc = stageSize*(stageSize-2)+7;
+let invadersLoc = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+];
+let gameInterval;
 
 function makePlayer(){
     stage[playerLoc].classList.add("player");
 }
-makePlayer()
 
 function movePlayer(e){
     stage[playerLoc].classList.remove("player");
     switch(e.keyCode){
-        case 37:
+        case 37: //왼쪽 화살표
             if(playerLoc % stageSize !== 0){
                 playerLoc--;
             }
             break;
-        case 39:
+        case 39: //오른쪽 화살표
             if(playerLoc % stageSize < stageSize-1){
                 playerLoc++;
             }
@@ -43,28 +48,56 @@ function movePlayer(e){
             break;
     }
     stage[playerLoc].classList.add("player");
-    console.log(playerLoc)
 }
 
-document.addEventListener("keyup",movePlayer)
-
-let invaderLoc = [
-    0,1,2,3,4,5,6,7,8,9,
-    15,16,17,18,19,20,21,22,23,24,
-    30,31,32,33,34,35,36,37,38,39
-]
-let gameInterval;
-
 function makeInvader(){
-    invaderLoc.forEach(function(invader){
+    invadersLoc.forEach(function(invader){
         stage[invader].classList.add("invader");
     })
 }
-makeInvader();
 
 function moveInvader(){
-
+    invadersLoc.forEach(function(invader){
+        stage[invader].classList.remove("invader");
+    })
+    for(let i=0; i < invadersLoc.length; i++){
+        invadersLoc[i]++;
+        stage[invadersLoc[i]].classList.add("invader");
+    }
 }
 
-interval = setInterval(moveInvader,1000)
+function gameStart(){
+    stage[playerLoc].classList.remove("player");
+    invadersLoc.forEach(function(invader){
+        stage[invader].classList.remove("invader");
+    });
+    playerLoc = stageSize*(stageSize-2)+7;
+    let invadersLoc = [
+        0,1,2,3,4,5,6,7,8,9,
+        15,16,17,18,19,20,21,22,23,24,
+        30,31,32,33,34,35,36,37,38,39
+    ]
+    makePlayer();
+    makeInvader();
+    displayStatus();
+    gameInterval = setInterval(moveInvader,1000);
+    document.addEventListener("keyup",movePlayer);
+    gameRun();
+}
+
+function gameStop(){
+    clearInterval(gameInterval);
+    document.removeEventListener("keyup",movePlayer);
+}
+
+function gameRun(){
+    moveInvader();
+}
+
+function displayStatus(){
+    display.innerText = invadersLoc.length + "/" + invadersLoc.length;
+}
+
+startBtn.addEventListener("click",gameStart);
+stopBtn.addEventListener("click",gameStop);
 
